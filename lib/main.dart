8c06bba39fd4bcc61097267e8093814300186f7d
Kishwar Jabeen <43556329+kishwerjabeen@users.noreply.github.com>
2021-07-30 23:42:43 +0500

    Day 3 complated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/login_page.dart';
-import './home_page.dart';
+import 'package:flutter_application_1/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
